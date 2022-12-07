44dc2f9aebe5ffa649ef009080b9a453c41cd587
RHT <2782299413@qq.com>
2022-12-07 17:43:26 +0800

    commit

--- tool/time.py
+++ tool/time.py
@@ -1,3 +1,10 @@
 
-def format_time_twitter(text):
-    return text
+import datetime
+
+from common.date import ENGLISH_MONTH
+
+
+def format_time_twitter(data1):
+    dl = data1.split(' ')
+    out = datetime.datetime(int(dl[5]),ENGLISH_MONTH[dl[1]],int(dl[2])).strftime("%Y-%m-%d ")
+    return out + dl[3]
